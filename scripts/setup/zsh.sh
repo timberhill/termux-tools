7e14ae7539bcb4fa9fe26c466b96c16ab8aa06f7
Maksym Lisogorskyi <m.lisogorskyi@gmail.com>
2022-01-03 20:49:05 +0000

    non-interactive zsh installation

--- scripts/setup/zsh.sh
+++ scripts/setup/zsh.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
 echo "...Installing oh my zsh"
-pkg install zsh
+pkg install --yes zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "...Installing powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/^ZSH_THEME\s*=\s*\(.*\)/ZSH_THEME="powerlevel10k\/powerlevel10k" #\1/g' ../.zshrc
 

c337717ef8493811137f37161d156ffe0e9c0ac3
Maksym Lisogorskyi <m.lisogorskyi@gmail.com>
2022-01-03 13:39:32 +0000

    separated zsh and git setup scripts

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -9,11 +9,3 @@ sed -i 's/^ZSH_THEME\s*=\s*\(.*\)/ZSH_THEME="powerlevel10k\/powerlevel10k" #\1/g
 echo "...Setting up git"
 pkg install openssh git
 
-ssh-keygen -t ed25519 -C "m.lisogorskyi@gmail.com"
-echo
-echo "...Add this key to github account and continue"
-echo "..."
-cat .ssh/id_ed25519.pub
-echo "..."
-
-git clone git@github.com:timberhill/termux-tools

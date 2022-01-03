afc999dba52390246976715316d644e5a162b2e2
Maksym Lisogorskyi <m.lisogorskyi@gmail.com>
2022-01-03 15:43:07 +0000

    fixed scriot separation bug, added done messages

--- setup-git.sh
+++ setup-git.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/bash
 
+echo "...Setting up git"
+pkg install openssh git
+
 ssh-keygen -t ed25519 -C "m.lisogorskyi@gmail.com"
 echo
-echo "...Add this key to github account and continue"                                                                  echo "..."
+echo "...Add this key to github account and continue"   
+echo "..."
 cat .ssh/id_ed25519.pub
 echo "..."
 
 git config --global user.email "m.lisogorskyi@gmail.com"
 git config --global user.name "Maksym Lisogorskyi"
+
+echo "...done"
 
--- setup-zsh.sh
+++ setup-zsh.sh
@@ -6,6 +6,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/^ZSH_THEME\s*=\s*\(.*\)/ZSH_THEME="powerlevel10k\/powerlevel10k" #\1/g' ../.zshrc
 
-echo "...Setting up git"
-pkg install openssh git
+echo "...done"
+echo "...Restart the terminal to apply"
 
